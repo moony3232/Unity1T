c9865e8360517ac7d71b759ae92c25b8842896c2
moony3232 <36894463+moony3232@users.noreply.github.com>
2020-10-31 12:53:57 +0100

    dsfs
    sdfsd

---
+++ MyFirstGame/Assets/script1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class script1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("W was pressed");
+        }
+        if (input.GetKeyDown(KeyCode.S))
+        {
+            Debug.Log("S was pressed");
+        }
+    }
+}
